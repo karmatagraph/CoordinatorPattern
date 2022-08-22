62bae6f6293ca16d24d0648a9322c72823b98f6a
karma <karmatashi954@gmail.com>
2022-08-22 09:45:19 +0545

    Initial Commit

---
+++ CoordinatorPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by karma on 8/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
